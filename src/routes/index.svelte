e4e714d8fdb474640b34432f1c649343a4ee3495
Mohammad Kefah <missa18@ku.edu.tr>
2021-06-27 18:20:36 +0300

    initial template for svelte with tailwindcss

---
+++ src/routes/index.svelte
@@ -0,0 +1,27 @@
+<div
+  class="flex flex-col justify-center items-center align-middle h-full w-full bg-gray-100"
+>
+  <div
+    class="
+             bg-gradient-to-tl
+             from-blue-300
+             to-red-200
+             drop-shadow-md
+             "
+  >
+    <p
+      class="
+             text-black/0
+             backdrop-blur-3xl
+             bg-gradient-to-tr
+             from-green-900
+             to-red-900
+             mix-blend-difference
+             bg-clip-text
+             font-black
+             p-4 text-4xl uppercase"
+    >
+      Hello World
+    </p>
+  </div>
+</div>
